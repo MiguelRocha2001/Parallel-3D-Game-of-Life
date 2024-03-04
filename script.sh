70da88670619bf656975f59e4dd30fc27f25d6af
Miguel Rocha <miguelasrocha1@gmail.com>
2024-03-04 17:18:38 +0000

    feat: adds times and bash script test

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for t in 8 4 2 1
+do
+    export OMP_NUM_THREADS=$t
+    ./life3d.o 1000 64 0.4 0 > tests/64_$t.txt
+    ./life3d.o 200 128 .5 1000 > tests/128_$t.txt
+    ./life3d.o 10 512 .4 0 > tests/512_$t.txt
+    ./life3d.o 3 1024 .4 100 > tests/1024_$t.txt
+done
