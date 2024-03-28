267af892f4374f1a22d55bf1c827c97459e1f57f
Miguel Rocha <miguelasrocha1@gmail.com>
2024-03-28 21:22:36 +0000

    fix: script and times

--- mpi/script_v2.sh
+++ mpi/script_v2.sh
@@ -3,18 +3,18 @@
 for t in 64 32 16 8
 do
     echo "Running test 1 with $t processes..."
-    srun -n $t --cpus-per-task=4 life3d 1000 64 .4 0 > slurm/output/$t_$64.txt
+    srun -n $t --cpus-per-task=1 -C lab2 life3d 1000 64 .4 0 > slurm/output/$t_$64.txt
     echo "Finished 64_$t.txt"
     
     echo "Running test 2 with $t processes..."
-    srun -n $t --cpus-per-task=4 life3d 200 128 .5 1000 > slurm/output/$t_$128.txt
+    srun -n $t --cpus-per-task=1 -C lab2 life3d 200 128 .5 1000 > slurm/output/$t_$128.txt
     echo "Finished 128_$t.txt"
     
     echo "Running test 3 with $t processes..."
-    srun -n $t --cpus-per-task=4 life3d 10 512 .4 0 > slurm/output/$t_$512.txt
+    srun -n $t --cpus-per-task=1 -C lab2 life3d 10 512 .4 0 > slurm/output/$t_$512.txt
     echo "Finished 512_$t.txt"
     
     echo "Running test 4 with $t processes..."
-    srun -n $t --cpus-per-task=4 life3d 3 1024 .4 100 > slurm/output/$t_$1024.txt
+    srun -n $t --cpus-per-task=1 -C lab2 life3d 3 1024 .4 100 > slurm/output/$t_$1024.txt
     echo "Finished 1024_$t.txt"
 done
